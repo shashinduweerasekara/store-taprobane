0e13b8ff6d45f99b5cba3082059d1d5761234d03
shashinduweerasekara <lakshan.shashindu@gmail.com>
2020-11-17 23:08:23 +0530

    quantity change code is changed

--- js/quantity.js
+++ js/quantity.js
@@ -1,5 +1,6 @@
 //default
 document.querySelector(".btn-min").setAttribute("disabled", "disabled");
+document.querySelector(".btn-min1").setAttribute("disabled", "disabled");
 
 //quantity
 var valueCount;
@@ -24,5 +25,28 @@ document.querySelector(".btn-plus").addEventListener("click", function () {
   if (valueCount > 1) {
     document.querySelector(".btn-min").removeAttribute("disabled");
     document.querySelector(".btn-min").classList.remove("disabled");
+  }
+});
+
+//- button
+document.querySelector(".btn-min1").addEventListener("click", function () {
+  valueCount = document.getElementById("quantity1").value;
+  valueCount--;
+  document.getElementById("quantity1").value = valueCount;
+
+  if (valueCount == 1) {
+    document.querySelector(".btn-min1").setAttribute("disabled", "disabled");
+  }
+});
+
+//+ button
+document.querySelector(".btn-plus1").addEventListener("click", function () {
+  valueCount = document.getElementById("quantity1").value;
+  valueCount++;
+  document.getElementById("quantity1").value = valueCount;
+
+  if (valueCount > 1) {
+    document.querySelector(".btn-min1").removeAttribute("disabled");
+    document.querySelector(".btn-min1").classList.remove("disabled");
   }
 });
